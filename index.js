de639fb6a74a7fb325dc19b41d5ca5d49a060820
cwright0428 <cwright0428@yahoo.com>
2019-03-15 14:55:13 -0500

    adds data

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const server = require('./api/server.js')
 
-server.listen(port, () => console.log(`\nServer running on PORT ${port}\n`));
+server.get('/', async (req, res) => {
+    res.status(200).json({ response: 'ready player one '})
+})
+
+server.get('/api/games', async (req, res) => {
+    const list = await data(games);
+    res.status(200).json(list);
+})
+
+server.post('/api/games', async (req, res) => {
+    const game = req.body;
+
+    if (game.title) {
+        const ids = await games.insert(game)
+        res.status(201).json(ids);
+    }
+    else {
+        res.status(422).json({error: 'title and genre required'})
+    }
+})
+
+const port = 9000;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
