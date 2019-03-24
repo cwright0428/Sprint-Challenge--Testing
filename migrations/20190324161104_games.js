78ed7c9f8bbe14bd72806000045732fae22e9eea
cwright0428 <cwright0428@yahoo.com>
2019-03-24 16:20:52 -0500

    added helpers

--- migrations/20190324161104_games.js
+++ migrations/20190324161104_games.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
-  
-};
+    return knex.schema.createTable('games', gameInfo => {
+        gameInfo.increments();
+        gameInfo.string('Title').notNullable().unique();
+        gameInfo.string('Genre').notNullable();
+        gameInfo.integer('Release Year').notNullable();
+    })
+  };
 
-exports.down = function(knex, Promise) {
-  
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('games');
+  };
